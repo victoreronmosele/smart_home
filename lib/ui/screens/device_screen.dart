0ac822fb01d9ca042714b77ac029074f3df7b171
Victor Eronmosele <victoreronmosele@gmail.com>
2022-06-29 00:17:30 +0100

    Add Cupertino back button to Device Screen

--- lib/ui/screens/device_screen.dart
+++ lib/ui/screens/device_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_home/enums/device.dart';
 
@@ -8,8 +9,30 @@ class DeviceScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(device.name),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: const Icon(CupertinoIcons.arrow_left, color: Colors.black,),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Text(
+                  device.name,
+                  style: const TextStyle(
+                    fontSize: 36.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
