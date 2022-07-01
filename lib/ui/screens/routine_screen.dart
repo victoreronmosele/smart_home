66eeb1491a113ec9c3b73686d27cb63735c8aadd
Victor Eronmosele <victoreronmosele@gmail.com>
2022-07-01 04:00:08 +0100

    Add status and edit button to Routine Screen

--- lib/ui/screens/routine_screen.dart
+++ lib/ui/screens/routine_screen.dart
@@ -28,6 +28,16 @@ class RoutineScreen extends StatelessWidget {
           routine.name,
           style: const TextStyle(color: Colors.black),
         ),
+        actions: [
+          /// Show Edit button if the routine is not a default one
+
+          if (!routine.isSystemRoutine)
+            const Center(
+                child: Padding(
+              padding: EdgeInsets.only(right: 16.0),
+              child: Text('Edit', style: TextStyle(color: Colors.black)),
+            )),
+        ],
       ),
       body: SafeArea(
           child: SingleChildScrollView(
@@ -35,31 +45,6 @@ class RoutineScreen extends StatelessWidget {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              const Text(
-                'Running on: ',
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-              const SizedBox(
-                height: 8.0,
-              ),
-              Text(
-                routineDevice.longName,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24.0,
-                ),
-              ),
-              Center(
-                  child: Image.asset(
-                routineDevice.imageUrl,
-                height: 300,
-                fit: BoxFit.fitWidth,
-              )),
-              const SizedBox(
-                height: 16.0,
-              ),
               const Text(
                 'Current Value:  ',
                 style: TextStyle(
@@ -83,6 +68,61 @@ class RoutineScreen extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                       fontSize: 32.0,
                       color: Colors.white),
+                ),
+              ),
+              const SizedBox(
+                height: 48.0,
+              ),
+              const Text(
+                'Running on: ',
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              Text(
+                routineDevice.longName,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24.0,
+                ),
+              ),
+              Center(
+                  child: Image.asset(
+                routineDevice.imageUrl,
+                height: 300,
+                fit: BoxFit.fitWidth,
+              )),
+              Text.rich(
+                TextSpan(
+                    text: 'Status: ',
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: '(${DisplayStringsUtil.getNormalLevelInformation(
+                          maximum: routine.normalLevel.maximum,
+                          minimum: routine.normalLevel.minimum,
+                          unit: routine.unit,
+                          showUnitFirst: routine.showUnitFirst,
+                          showDecimal: routine.showDecimal,
+                        )})',
+                        style: const TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                    ]),
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              Text(
+                routine.status.title,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                  color: routine.status.color,
                 ),
               ),
             ],
